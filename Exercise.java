312c821fb2e85690540203139917b28fb111b259
Your Name <you@example.com>
2026-02-10 15:33:11 +0530

    completedtill methods

---
+++ Exercise.java
@@ -0,0 +1,38 @@
+import java.lang.System;
+class exercise{
+    public static void main(String[]args)
+    {
+        String name="Vidyavahini";
+        int age=20;
+        float height=5.5f;
+        double weight=60.5;
+        System.out.println("Name:"+name);
+        System.out.println("Age:"+age);
+        System.out.println("Height:"+height);
+        System.out.println("Weight:"+weight);
+    }
+}
+
+class exercise2{
+    public static void main(String[]args)
+    {
+        int a=10;
+        int b=20;
+        System.out.println("The sum of two integers is"+(a+b));
+        System.out.println("The diff of two integers is"+(a-b));
+        System.out.println("The product of two integers is"+(a*b));
+        System.out.println("The division of two integers is"+(a/b));
+
+    }
+}
+
+class exercise3
+{
+    public static void main(String[]args)
+    {
+        int pin=572104;
+        String name="Tumkur";
+        System.out.println("The pincode is"+pin);
+        System.out.println("The city is"+name);
+    }
+}
