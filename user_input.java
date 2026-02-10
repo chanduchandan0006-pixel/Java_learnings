cf6c9e196acb05dc302eeb8ef0ae88a884ca69e8
Your Name <you@example.com>
2026-02-10 12:48:37 +0530

    completed till user input

---
+++ user_input.java
@@ -0,0 +1,25 @@
+import java.lang.System;
+import java.util.Scanner;
+
+class user_input_int{
+    public static void main(String[]args){
+        Scanner scn =new Scanner (System.in);
+        int a=scn.nextInt();
+        System.out.println("The value of a is:"+a);
+    }
+}
+
+class user_input_string{
+    public static void main(String[]args){
+        Scanner scn =new Scanner (System.in);
+        String name=scn.nextInt();
+        System.out.println("The value of a is:"+name);
+    }
+}
+
+class addition_of_two_numbers{
+    public static void main(String[]args){
+        Scanner scn=new Scanner (System.in);
+        int a=
+    }
+}
